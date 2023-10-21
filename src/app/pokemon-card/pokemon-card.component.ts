7dc5f55f4aa7b5b0f3b7bba2a19694ed0d353aa0
Benny Nobre <bennynobre1@gmail.com>
2023-10-21 18:15:54 -0300

    Adding static pokemons from API

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Pokemon } from '../../_model/Pokemon';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.sass']
+})
+export class PokemonCardComponent {
+  @Input()
+  public pokemon!: Pokemon;
+
+  public leadingZero(str: string | number, size: number): string{
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+
+    return s;
+  }
+}
